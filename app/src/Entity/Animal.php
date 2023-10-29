48e5678409a2562dda27093ca4d531a10d542664
endricklosange <69051334+endricklosange@users.noreply.github.com>
2023-10-29 18:14:11 +0100

    Adding the symfony project and .gitignore file

---
+++ app/src/Entity/Animal.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnimalRepository::class)]
+class Animal
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $legs = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLegs(): ?string
+    {
+        return $this->legs;
+    }
+
+    public function setLegs(string $legs): static
+    {
+        $this->legs = $legs;
+
+        return $this;
+    }
+}
